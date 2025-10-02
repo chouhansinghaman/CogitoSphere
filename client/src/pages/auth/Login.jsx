41838fc5c4af3dc525314b06ea72b12ac028a70d
Aman Singh Chouhan <88761235+chouhansinghaman@users.noreply.github.com>
2025-10-02 23:01:26 +0530

    Update Login.jsx

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import toast from "react-hot-toast";
-import { loginApi } from "../../api/auth";
+import { loginApi } from "../../services/apo.auth.js";
 import Loader from "../../components/Loader";
 import { useMinimumLoadingTime } from "../../hooks/useMinimumLoadingTime";
 
