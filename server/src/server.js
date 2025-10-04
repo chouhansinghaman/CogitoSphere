aa731b84719da9eae40d9438f2338cbf1896e076
Aman Singh Chouhan <88761235+chouhansinghaman@users.noreply.github.com>
2025-10-04 16:13:54 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -25,7 +25,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  process.env.FRONTEND_URL
+  process.env.VITE_FRONTEND_URL
 ].filter(Boolean);
 
 // CORS middleware
