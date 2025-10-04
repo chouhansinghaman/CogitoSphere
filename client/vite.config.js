33f297e87795b9588f31feb95cc34fbbbe0c5dc4
Aman Singh Chouhan <88761235+chouhansinghaman@users.noreply.github.com>
2025-10-04 20:53:05 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: './',
   server: {
     historyApiFallback: true,
-=======
   base: './', // makes asset paths relative (important for Vercel)
   server: {
     historyApiFallback: true, // enables React Router fallback in dev
   },
   preview: {
     historyApiFallback: true, // ensures the same in vite preview
->>>>>>> aa731b84719da9eae40d9438f2338cbf1896e076
   },
 })
