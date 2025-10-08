6637cb2b179df8533bb66c88c2df86691b7f6cc1
Aman Singh Chouhan <88761235+chouhansinghaman@users.noreply.github.com>
2025-10-08 09:08:59 +0530

    Update api.js

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,23 +2,12 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true
 });
 
-// Attach token automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("authToken");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
-
-// Handle errors uniformly
-API.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const msg = error?.response?.data?.message || error.message || "Request failed";
-    return Promise.reject(new Error(msg));
-  }
-);
 
 export default API;
