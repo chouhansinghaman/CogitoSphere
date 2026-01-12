b7a87b0c3cf257db24e4b22565d3329b58c7271a
Aman Singh Chouhan <88761235+chouhansinghaman@users.noreply.github.com>
2026-01-12 16:34:17 +0000

    Fixed /reset-password path in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -55,7 +55,7 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/reset-password/" element={<ResetPassword />} />
 
           {/* Private Routes */}
           <Route element={<PrivateRoute />}>
